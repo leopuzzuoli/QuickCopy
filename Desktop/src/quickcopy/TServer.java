cc9c9a625e24fbe0b3ed8db351d5fcaef50995e5
Leonardo Puzzuoli <chipleo.codes@gmail.com>
2019-09-13 16:31:00 +0200

    .

--- Desktop/src/quickcopy/TServer.java
+++ Desktop/src/quickcopy/TServer.java
@@ -74,105 +74,103 @@ public class TServer extends Thread {
         while (running) {
             try {
                 clientSocket = serverSocket.accept();
-                out = new PrintWriter(clientSocket.getOutputStream(), true);
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                String received = in.readLine();
-                //if recieved is reply to scan
-                System.out.println("<- " + received);
-                if (received.startsWith("QC responding from ")) {
-                    //add connection
-                    String[] i_i = received.split("QC responding from ");
-                    String[] all = i_i[1].split(":");
+                Thread connectionHandler = new Thread() {
+                    @Override
+                    public void run() {
+                        try{
+                        out = new PrintWriter(clientSocket.getOutputStream(), true);
+                        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                        String received = in.readLine();
+                        //if recieved is reply to scan
+                        System.out.println("<- " + received);
+                        if (received.startsWith("QC responding from ")) {
+                            //add connection
+                            String[] i_i = received.split("QC responding from ");
+                            String[] all = i_i[1].split(":");
 
-                    Connection newConn = new Connection(all[0], Integer.parseInt(all[1]));
-                    //checks should be made for name
-                    newConn.setName(all[2]);
-
-                    Platform.runLater(new Runnable() {
-                        @Override
-                        public void run() {
-                            // Update UI here.
-                            MainController.addConnection(newConn, mc);
-                        }
-                    });
-                } else if (received.startsWith("msg")) {
-                    //handle recieved message
-                    System.out.println(received.substring(3));
-                } else if (received.startsWith("Accept")) {
-                    System.out.println(received.substring(6));
-
-                    //see who is sending the message
-                    Thread t1 = new Thread() {
-
-                        public void run() {
+                            Connection newConn = new Connection(all[0], Integer.parseInt(all[1]));
+                            //checks should be made for name
+                            newConn.setName(all[2]);
 
                             Platform.runLater(new Runnable() {
                                 @Override
                                 public void run() {
-                                    String sender = "A user ";
-                                    List<Connection> conns = MainController.getConnections();
-                                    for (Connection c : conns) {
-                                        if (clientSocket.getInetAddress().toString().substring(1).equals(c.getAddr())) {
-                                            sender = c.getName() + " ";
-                                        }
-                                    }
-                                    //ask user if he wants to accept the files
-                                    Alert alert = new Alert(AlertType.CONFIRMATION);
-                                    alert.setTitle("Request received");
-                                    alert.setHeaderText(sender + "wants to send you these files, Accept?");
-                                    alert.setContentText(received.substring(6));
-                                    ButtonType buttonTypeAccept = new ButtonType("Accept");
-                                    ButtonType buttonTypeCancel = new ButtonType("Refuse", ButtonBar.ButtonData.CANCEL_CLOSE);
-
-                                    alert.getButtonTypes().setAll(buttonTypeAccept, buttonTypeCancel);
-                                    Optional<ButtonType> result = alert.showAndWait();
-                                    //1 accept 0 refuse
-                                    if (result.get() == buttonTypeAccept) {
-                                        out.println("green");
-                                        System.out.println("green");
-                                        //add accepted files to list of accepted files
-                                        acceptedFiles.addAll(Arrays.asList(received.substring(7, received.length() - 1).split(", ")));
-                                    } else {
-                                        out.println("red");
-                                    }
+                                    // Update UI here.
+                                    MainController.addConnection(newConn, mc);
                                 }
-
                             });
+                        } else if (received.startsWith("msg")) {
+                            //handle recieved message
+                            System.out.println(received.substring(3));
+                        } else if (received.startsWith("Accept")) {
+                            System.out.println(received.substring(6));
+
+                            //see who is sending the message
+                                    Platform.runLater(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            String sender = "A user ";
+                                            List<Connection> conns = MainController.getConnections();
+                                            for (Connection c : conns) {
+                                                if (clientSocket.getInetAddress().toString().substring(1).equals(c.getAddr())) {
+                                                    sender = c.getName() + " ";
+                                                }
+                                            }
+                                            //ask user if he wants to accept the files
+                                            Alert alert = new Alert(AlertType.CONFIRMATION);
+                                            alert.setTitle("Request received");
+                                            alert.setHeaderText(sender + "wants to send you these files, Accept?");
+                                            alert.setContentText(received.substring(6));
+                                            ButtonType buttonTypeAccept = new ButtonType("Accept");
+                                            ButtonType buttonTypeCancel = new ButtonType("Refuse", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+                                            alert.getButtonTypes().setAll(buttonTypeAccept, buttonTypeCancel);
+                                            Optional<ButtonType> result = alert.showAndWait();
+                                            //1 accept 0 refuse
+                                            if (result.get() == buttonTypeAccept) {
+                                                out.println("green");
+                                                System.out.println("green");
+                                                //add accepted files to list of accepted files
+                                                acceptedFiles.addAll(Arrays.asList(received.substring(7, received.length() - 1).split(", ")));
+                                            } else {
+                                                out.println("red");
+                                            }
+                                        }
+
+                                    });
+                                
+
+                        } else if (received.startsWith("file")) {
+                            //get data
+                            String filename = received.split(" ")[1];
+                            String _filesize = received.split(" ")[2];
+                            int filesize = Integer.parseInt(_filesize);
+
+                            DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+                            FileOutputStream fos = new FileOutputStream(PATH + filename);
+                            byte[] buffer = new byte[4096];
+
+                            int read = 0;
+                            int totalRead = 0;
+                            int remaining = filesize;
+                            while ((read = dis.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
+                                totalRead += read;
+                                remaining -= read;
+                                //TODO: make into loading bar
+                                System.out.println("read " + totalRead + " bytes.");
+                                fos.write(buffer, 0, read);
+                            }
+                            System.out.println("dojne: " + filename);
+                            fos.close();
+                            dis.close();
                         }
-                    };
-                    t1.start();
-                    try{
-                    t1.join();
-                    System.out.println("Thread Completed");
                     }
-                    catch(InterruptedException e){
-                        System.out.println("Thread interrupted");
-                    }
-
-                } else if (received.startsWith("file")) {
-                    //get data
-                    String filename = received.split(" ")[1];
-                    String _filesize = received.split(" ")[2];
-                    int filesize = Integer.parseInt(_filesize);
-
-                    DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
-                    FileOutputStream fos = new FileOutputStream(PATH + filename);
-                    byte[] buffer = new byte[4096];
-
-                    int read = 0;
-                    int totalRead = 0;
-                    int remaining = filesize;
-                    while ((read = dis.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
-                        totalRead += read;
-                        remaining -= read;
-                        //TODO: make into loading bar
-                        System.out.println("read " + totalRead + " bytes.");
-                        fos.write(buffer, 0, read);
-                    }
-                    System.out.println("dojne: " + filename);
-                    fos.close();
-                    dis.close();
+                        catch(IOException | NumberFormatException e){
+                    System.out.println("communication error inside Thread: " + e.toString());
                 }
+                    }
+                };
+                        connectionHandler.start();
             } catch (IOException | NumberFormatException e) {
                 System.out.println("communication error: " + e.toString());
             }
